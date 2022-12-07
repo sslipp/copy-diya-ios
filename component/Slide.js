6d36ece270f6eb8f94fec0dc3c4096aa144a4f74
SunWebRod <116952965+SunWebRod@users.noreply.github.com>
2022-12-07 13:45:43 -0800

    Add files via upload

--- component/Slide.js
+++ component/Slide.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 import CarouselPager from 'react-native-carousel-pager';
 import Swipers from './Swipers';
 import Swipers2 from './Swipers2';
 import Swipers3 from './Swipers3';
 import { LogBox } from 'react-native';
+import Dots from 'react-native-dots-pagination';
 
 export default class Slide extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: 0
+        }
+    }
 
     render() {
         LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
         return (
             <View style={{ flex: 1 }}>
-                <CarouselPager ref={ref => this.carousel = ref} blurredOpacity={1} blurredZoom={0.85}>
+                <CarouselPager ref={ref => this.carousel = ref} blurredOpacity={1} blurredZoom={0.85} onPageChange={i => this.setState({ active: i })}>
                     <View key={'page0'}>
                         <Swipers Fam={this.props.Fam} Name={this.props.Name} Otch={this.props.Otch} Date={this.props.Date} gender={this.props.gender} image={this.props.image} pickImage={this.props.pickImage} />
                     </View>
@@ -23,6 +30,7 @@ export default class Slide extends Component {
                         <Swipers3 Fam={this.props.Fam} Name={this.props.Name} Otch={this.props.Otch} Date={this.props.Date} gender={this.props.gender} image={this.props.image} pickImage={this.props.pickImage} />
                     </View>
                 </CarouselPager>
+                <Dots length={3} active={this.state.active} activeColor='#0A0603' passiveColor='#7E8A81' passiveDotWidth={8} passiveDotHeight={8} activeDotHeight={10} activeDotWidth={10} />
             </View>
         );
     }
