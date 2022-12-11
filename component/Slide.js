35ad0b03c5d4bd30f881adb42817c8d0552c9d9b
SunWebRod <116952965+SunWebRod@users.noreply.github.com>
2022-12-11 13:57:50 -0800

    Add files via upload

--- component/Slide.js
+++ component/Slide.js
@@ -53,7 +53,7 @@ export default class Slide extends Component {
                         <Swipers3 Fam={this.props.Fam} Name={this.props.Name} Otch={this.props.Otch} Date={this.props.Date} gender={this.props.gender} image={this.props.image} pickImage={this.props.pickImage} bgCol3={this.props.bgCol3} visibleElement3={this.props.visibleElement3} />
                     </View>
                 </CarouselPager>
-                <Dots length={3} active={this.state.active} paddingVertical={24.5} activeColor='#0A0603' passiveColor='#7E8A81' passiveDotWidth={5} passiveDotHeight={5} activeDotHeight={7} activeDotWidth={7} />
+                <Dots length={3} active={this.state.active} paddingVertical={24.5} activeColor='#0A0603' passiveColor='rgba(0, 0, 0, 0.3)' passiveDotWidth={5} passiveDotHeight={5} activeDotHeight={7} activeDotWidth={7} />
             </View>
         );
     }
